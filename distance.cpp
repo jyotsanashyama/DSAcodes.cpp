fd0c45f2eb2d752e5a4a2870d4fc732289d5e613
jyotsanashyama <63206848+jyotsanashyama@users.noreply.github.com>
2022-10-05 12:21:31 +0530

    Add files via upload

---
+++ distance.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+int main() {
+
+  double dist;
+  double diste;
+
+std::cout << "Enter the distance in miles\n";
+std::cin >> diste;
+diste = dist*1.6;
+std::cout << "The required distance in kilometers is:" << diste << "\n";
+
+}
---
+++ helloworld.cpp
@@ -0,0 +1,9 @@
+#include<iostream>
+
+int main(){
+
+std::cout <<"Hello World! I am Jyotsana Shyama.\
+n";
+
+
+}
